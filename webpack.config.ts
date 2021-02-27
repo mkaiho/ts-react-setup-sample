767f23b61a6101c389967e08317ebcd3a7280ecd
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2021-02-27 23:31:49 +0900

    add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import * as path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+};
+
+export default config;
